16feabd565f2108a60b86b2382324b473049b48c
lorinho404 <94736865+lorinho404@users.noreply.github.com>
2021-11-20 22:31:12 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,21 @@
+ 
+ ​menu​ ​=​ ​''' ​\033​[31m 
+ ​┏━╸╻ ╻┏━┓┏━┓┏━┓   ╻ ╻┏━┓╻ ╻ 
+ ​┃  ┃ ┃┣┳┛┗━┓┃ ┃   ┗━┫┃┃┃┗━┫ 
+ ​┗━╸┗━┛╹┗╸┗━┛┗━┛     ╹┗━┛  ╹ 
+ ​\033​[0m 
+ 
+  CURSO DERMUX 404: By LORINHO404
+    TEAM 404 DOMINA
+  yumi_perfeita
+  
+ ​[ 1 ] REDHAWK CURSO TERMUX 
+ ​[ 2 ] SQLMAP CURSO TERMUX 
+ ​[ 3 ] DDOS CURSO TERMUX 
+ ​[ 4 ] COMANDOS INICIAIS CURSO TERMUX 
+ ​[ 5 ] DOWLOAD PDF CURSO TERMUX 
+ ​[ 6 ] NOSSO GRUPO WPP CURSO TERMUX 
+ ​[ 7 ] CURSO 404 VPN 
+ ​[ 8 ] CURSO 404 COMANDOS EXPLICAÇÃO 
+ ​[ 9 ] SAIR 
+ ​'''
